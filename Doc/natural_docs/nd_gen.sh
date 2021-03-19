dd486326592430bbfcbb2eab4a4062e550f0f27f
Syed Asad Alam <alam.asad@gmail.com>
2021-03-19 02:27:12 +0000

    script for ND updated

--- Doc/natural_docs/nd_gen.sh
+++ Doc/natural_docs/nd_gen.sh
@@ -4,13 +4,13 @@ export mvau_root=`pwd | sed "s?/Doc/natural_docs??"`
 export out_dir=$mvau_root/Doc/api
 export src_dir=$mvau_root/proj/src
 rm -rf $out_dir
-sed "s?MVAU_ROOT?$mvau_root?g" $mvau_root/Doc/natural_docs/Proj/MVAU_Menu.txt > $mvau_root/Doc/natural_docs/Proj/Menu.txt
-cp $mvau_root/Doc/natural_docs/Proj/MVAU_Topics.txt $mvau_root/Doc/natural_docs/Proj/Topics.txt
+sed "s?MVAU_ROOT?$mvau_root?g" $mvau_root/Doc/natural_docs/Proj/MVAU_Project.txt > $mvau_root/Doc/natural_docs/Proj/Project.txt
+cp $mvau_root/Doc/natural_docs/Proj/MVAU_Comments.txt $mvau_root/Doc/natural_docs/Proj/Comments.txt
 cp $mvau_root/Doc/natural_docs/Proj/MVAU_Languages.txt $mvau_root/Doc/natural_docs/Proj/Languages.txt
 chmod 666 $mvau_root/Doc/natural_docs/Proj/Languages.txt
-chmod 666 $mvau_root/Doc/natural_docs/Proj/Menu.txt     
-chmod 666 $mvau_root/Doc/natural_docs/Proj/Topics.txt   
+chmod 666 $mvau_root/Doc/natural_docs/Proj/Project.txt
+chmod 666 $mvau_root/Doc/natural_docs/Proj/Comments.txt
 
 mkdir -p $out_dir
-perl $WITH_ND -do -ro -i $src_dir -o FramedHTML $out_dir -p Proj/
+mono $WITH_ND -do -ro -o FramedHTML $out_dir -p Proj/
 
