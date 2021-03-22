371a9f384b3c0b790aab21fb690f64be423e8297
Syed Asad Alam <alam.asad@gmail.com>
2021-03-22 10:44:00 +0000

    updated project file

--- Doc/natural_docs/nd_gen.sh
+++ Doc/natural_docs/nd_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -f
 
 export mvau_root=`pwd | sed "s?/Doc/natural_docs??"`
-export out_dir=$mvau_root/Doc/api
+export out_dir=$mvau_root/docs
 export src_dir=$mvau_root/proj/src
 rm -rf $out_dir
 sed "s?MVAU_ROOT?$mvau_root?g" $mvau_root/Doc/natural_docs/Proj/MVAU_Project.txt > $mvau_root/Doc/natural_docs/Proj/Project.txt
