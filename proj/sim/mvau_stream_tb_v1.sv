a189ad24cdf9835c5500a5375c93abcd48547482
Syed Asad Alam <alam.asad@gmail.com>
2021-03-22 02:18:23 +0000

    new files

--- proj/sim/mvau_stream_tb_v1.sv
+++ proj/sim/mvau_stream_tb_v1.sv
@@ -1,11 +1,10 @@
-/*******************************************************************************
+/*
+ * Testbench: mvau_stream_tb_v1.sv
  *
- * Authors: Syed Asad Alam <syed.asad.alam@tcd.ie>
- * \file mvu.sv
- *
+ *  Authors: Syed Asad Alam <syed.asad.alam@tcd.ie>
+ * 
  * This file lists a test bench for the matrix-vector activation streaming
- * unit
- * It is part of the Xilinx FINN open source framework for implementing
+ * unit It is part of the Xilinx FINN open source framework for implementing
  * quantized neural networks on FPGAs
  *
  * This material is based upon work supported, in part, by Science Foundation
@@ -13,11 +12,19 @@
  * European Union's Horizon 2020 research and innovation programme under the 
  * Marie Sklodowska-Curie grant agreement Grant No.754489. 
  * 
- *******************************************************************************/
+ * Parameters:
+ * 
+ * CLK_PER - Clock cycle period
+ * INIT_DLY=(CLK_PER*2)+1 - Initial delay
+ * RAND_DLY=21 - Random delay to be used when needed
+ * NO_IN_VEC = 100 - Number of input vectors
+ * ACT_MatrixW = OFMDim*OFMDim; - Input activation matrix height
+ * ACT_MatrixH = (KDim*KDim*IFMCh) - Input activation matrix weight
+ * */
 
 `timescale 1ns/1ns
 
-`include "../src/mvau_defn.sv" // compile the package file
+`include "../src/mvau_top/mvau_defn.sv" // compile the package file
 
 module mvau_stream_tb;
 
@@ -30,14 +37,45 @@ module mvau_stream_tb;
    parameter int ACT_MatrixH = (KDim*KDim*IFMCh); // input activation matrix weight
    
    // Signals Declarations
+   // Signals Declarations
+   // Signal: clk
+   // Main clock
    logic 	 clk;
+   // Signal: rst_n
+   // Asynchronous active low reset
    logic 	 rst_n;
+   // Signal: weights
+   // The weight matrix of dimensions MatrixW x MatrixH of word length TW
    logic [TW-1:0] weights [0:MatrixH-1][0:MatrixW-1];
+   // Signal: out_v
+   // Output valid signal
+   logic 	  out_v;
+   // Signal: out
+   // Output from DUT
    logic [TO-1:0] out;
+   // Signal: out_packed
+   // Output signal from DUT where each element is divided into multiple elements
+   // as DUT produces a packed output of size PE x TDstI
+   // Dimension: PE, word length: TDstI
    logic [0:PE-1][TDstI-1:0] out_packed;
-   logic [0:SIMD-1][TSrcI-1:0] in_act;
+   // Signal: in_v
+   // Input valid
+   logic 		     in_v;
+   // Signal: in_wgt
+   // Input weight stream to DUT
+   // Dimension: PExSIMD, word length: TW
    logic [0:SIMD-1][TW-1:0]    in_wgt[0:PE-1];
+   // Signal: in_mat
+   // Input activation matrix
+   // Dimension: ACT_MatrixH x ACT_MatrixW, word length: TSrcI
    logic [TSrcI-1:0] 	       in_mat [0:ACT_MatrixH-1][0:ACT_MatrixW-1];
+   // Signal: in_act
+   // Input activation stream to DUT
+   // Dimension: SIMD, word length: TSrcI
+   logic [0:SIMD-1][TSrcI-1:0] in_act;
+   // Signal: mvau_beh
+   // Output matrix holding output of behavioral simulation
+   // Dimension: MatrixH x ACT_MatrixW
    logic [TDstI-1:0] 	       mvau_beh [0:MatrixH-1][0:ACT_MatrixW-1];
    
    // Events for synchronizing the simulation
@@ -46,8 +84,9 @@ module mvau_stream_tb;
    event 		       do_mvau_beh;// perform behavioral mvau
    event 		       test_event; // to start testing
    
-   
-   //Generating Clock and Reset
+   // Initial: CLK_RST_GEN
+   // Initialization of Clock and Reset and performing validation
+   // between output of DUT and behavioral model
    initial
      begin
 	$display($time, " << Starting Simulation >>");	
@@ -71,40 +110,39 @@ module mvau_stream_tb;
 	// We need to delay more until the final output comes
 	// To-do for tomorrow
 	for(int i = 0; i < ACT_MatrixW; i++) begin
-	   for(int j = 0; j < MatrixH/PE; j++)
-	     begin
-		#(CLK_PER*MatrixW/SIMD)
-		out_packed = out;
-		@(posedge clk) begin		  
-		   for(int k = 0; k < PE; k++) begin
-		      if(out_packed[k] == mvau_beh[j*PE+k][i])
-			$display($time, "<< PE%d : 0x%0h >>, << Model_%d_%d: 0x%0h",k,out_packed[k],j*PE+k,i,mvau_beh[j*PE+k][i]);
-		      else begin
+	   for(int j = 0; j < MatrixH/PE; j++) begin
+	      #(CLK_PER*MatrixW/SIMD)
+	      out_packed = out;
+	      @(posedge clk) begin
+		 if(out_v) begin
+		    for(int k = 0; k < PE; k++) begin
+		       if(out_packed[k] == mvau_beh[j*PE+k][i])
 			 $display($time, "<< PE%d : 0x%0h >>, << Model_%d_%d: 0x%0h",k,out_packed[k],j*PE+k,i,mvau_beh[j*PE+k][i]);
-			 assert (out_packed[k] == mvau_beh[j*PE+k][i])
-			   else
-			     $fatal(1,"Data MisMatch");
-		      end			
-		   end
-		end
-	     end // for (int j = 0; j < MatrixH/PE-1; j++)
+		       else begin
+			  $display($time, "<< PE%d : 0x%0h >>, << Model_%d_%d: 0x%0h",k,out_packed[k],j*PE+k,i,mvau_beh[j*PE+k][i]);
+			  assert (out_packed[k] == mvau_beh[j*PE+k][i])
+			    else
+			      $fatal(1,"Data MisMatch");
+		       end			
+		    end
+		 end
+	      end
+	   end // for (int j = 0; j < MatrixH/PE-1; j++)
 	end // for (int i = 0; i < ACT_MatrixW-1; i++)	
-
+	
 	#RAND_DLY;
 	$display($time, "<< Simulation Complete >>");
 	$stop;		
      end // initial begin
 
    
-   /*
-    * Clock generation
-    * */
+   // Always: CLK_GEN
+   // Generating clock using the CLK_PER as clock period
    always
      #(CLK_PER/2) clk = ~clk;
 
-   /*
-    * Weight Matrix Generation
-    * */
+   // Always: WGT_MAT_GEN
+   // Generates a weight matrix using random data
    always @(gen_weights)
      begin
 	for(int row = 0; row < MatrixH; row=row+1)
@@ -112,9 +150,8 @@ module mvau_stream_tb;
 	    weights[row][col] = TW'($random);
      end
    
-   /*
-    * Input activation matrix generation
-    * */
+   // Always: INP_MAT_GEN
+   // Input matrix generation using random data
    always @(gen_inp)
      begin
 	for(int row = 0; row < ACT_MatrixH; row=row+1)
@@ -203,23 +240,29 @@ end
 	end
 end
    
-   /*
-    * Generating data from DUT
-    * */
+   // Always: INP_ACT_GEN_DUT
+   // Generating input activation stream data for DUT
+   // After generating one vector, waits for the input
+   // vector to be re-used for all weight matrix rows
    always @(test_event)   
      begin
 	for(int i = 0; i < ACT_MatrixW; i++) begin
 	   for(int j = 0; j < ACT_MatrixH/SIMD; j++) begin
 	      #(CLK_PER/2);
+	      in_v = 1'b1;
 	      for(int k = 0; k < SIMD; k++) begin
 		 in_act[k] = in_mat[j*SIMD+k][i];		 
 	      end
 	      $display($time, " << Row: %d, Col%d => Data In: 0x%0h >>", j,i,in_act);
 	      #(CLK_PER/2);	   
 	   end
+	   //#(CLK_PER/2);
+	   in_v = 1'b0;
 	   #(CLK_PER*((MatrixW/SIMD)*(MatrixH/PE-1)));
 	end
      end // always @ (test_event)
+   // Always: WGT_GEN_DUT
+   // Generating weight stream for DUT
    always @(test_event)
      begin
 	for(int x = 0; x < ACT_MatrixW; x++) begin
@@ -243,8 +286,10 @@ end
    mvau_stream mvau_stream_inst(
 				.rst_n,
 				.clk,
+				.in_v,
 				.in_act,
 				.in_wgt,
+				.out_v,
 				.out);
    
 endmodule // mvau_stream_tb
