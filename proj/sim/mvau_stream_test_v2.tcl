ba7a181c5de9d0ad8a405e41e72642eed6356c5b
Syed Asad Alam <alam.asad@gmail.com>
2021-04-28 03:21:45 +0100

    old files removed

--- proj/sim/mvau_stream_test_v2.tcl
+++
@@ -1,2 +0,0 @@
-exec xelab -prj mvau_stream_files.prj -s run_mvau_stream work.mvau_stream_tb_v2 --debug all
-exec xsim run_mvau_stream -gui -wdb mvau_stream_tb_v2.wdb -view mvau_stream_tb_v2.wcfg -t mvau_xsim.tcl
--- proj/sim/mvau_test_v2.tcl
+++
@@ -1,2 +0,0 @@
-exec xelab -prj mvau_files.prj -s run_mvau work.mvau_tb_v2 --debug all
-exec xsim -gui run_mvau -wdb mvau_tb_v2.wdb -view mvau_tb_v2.wcfg -t mvau_xsim.tcl
