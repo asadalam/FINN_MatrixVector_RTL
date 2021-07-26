d219f0975b43d6824de17939f81b39488267c5b2
Syed Asad Alam <alam.asad@gmail.com>
2021-07-26 22:49:10 +0100

    sim file for v5 test bench

---
+++ proj/sim/mvau_test_v5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf mvau_tb_v5.wdb
+make clean
+DO_GUI="gui"
+if [ "$1" == "$DO_GUI" ]; then
+    xelab -prj mvau_files.prj -s run_mvau_v5 work.mvau_tb_v5 --debug all
+    if [ $? -eq 0 ]; then
+	echo "RTL files compilation successfull"
+    else
+	echo "RTL files compilation failed"
+	exit 0
+    fi
+    xsim run_mvau_v5 -gui -wdb mvau_tb_v5.wdb -t mvau_xsim_gui.tcl --sv_seed $RANDOM
+else
+    xelab -prj mvau_files.prj -s run_mvau_v5 work.mvau_tb_v5
+    if [ $? -eq 0 ]; then
+	echo "RTL files compilation successfull"
+    else
+	echo "RTL files compilation failed"
+	exit 0
+    fi
+    xsim run_mvau_v5 -t mvau_xsim.tcl --sv_seed $RANDOM
+fi
+exit 1
